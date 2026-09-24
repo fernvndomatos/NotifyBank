7e1a3e8658da0b74ae1f702f6f2797acfe96a8c5
Fernando Matos <fernando.matos221@gmail.com>
2026-09-24 16:03:04 -0300

    chore: setup inicial do projeto NotifyBank

---
+++ src/main/java/com/github/fernvndomatos/NotifyBank/NotifyBankApplication.java
@@ -0,0 +1,13 @@
+package com.github.fernvndomatos.NotifyBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fernvndomatos/NotifyBank/NotifyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fernvndomatos.NotifyBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
